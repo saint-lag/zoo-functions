58866186e6d5306958beda030744d19f23e17f3e
saint-lag <maiasena.gabriel@gmail.com>
2022-01-27 11:21:24 -0300

    Function: getOldestFromFirstSpecies PASS

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,6 +2,21 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const { species } = data;
+  const { employees } = data;
+
+  const targetEmployeeObj = employees.find((employee) => employee.id === id);
+  const firstSpeciesInChargeId = targetEmployeeObj.responsibleFor[0];
+  const firstSpeciesObj = species.find(
+    (item) => item.id === firstSpeciesInChargeId
+  );
+
+  const reducer = (previous, current) =>
+    current.age > previous.age ? current : previous;
+
+  const { name, sex, age } = firstSpeciesObj.residents.reduce(reducer);
+
+  return [name, sex, age];
 }
 
 module.exports = getOldestFromFirstSpecies;
